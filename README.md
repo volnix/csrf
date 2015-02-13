aa3315aeb769758bb4d84b621260cfa25f2edabc
Nick Volgas <nvolgas@ark.org>
2015-02-13 16:06:18 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# csrf
-CSRF protection library that compares provided token to session token to ensure request validity
+# Volnix\CSRF
+
+CSRF protection library that compares provided token to session token to ensure request validity.  Token values are stored in session and validated against that session store to verify the request.  Tokens are changed on every request so ensure frequently changes and increase the difficulty in guessing the token value.
+
+### Installation
+
+This package is installed via composer and is pulled in as the `volnix/csrf` package.
+
+```json
+
+"require": {
+    "volnix/csrf": "~1.0"
+}
+
+```
+
+### Unit Tests
+
+The unit tests for this application live in the `./tests` directory.  There is a test for each library in the security package.  The PHPUnit xml file is in the root of the project (`./phpunit.xml`).
+
+### Usage
+
+This library is used for preventing cross-site request forgery.  Currently you can retrieve the token, a pre-built query string, or a pre-built hidden input tag.
+
+Value only:
+
+```php
+	
+<?php
+
+require_once('vendor/autoload.php');
+
+if (\Volnix\CSRF\CSRF::valid()) {
+	// valid request
+}
+else {
+	// invalid CSRF
+}
+
+?>
+
+<form action="index.php" method="post">
+	<input type="hidden" name="<?= \Volnix\CSRF\CSRF::TOKEN_NAME ?>" value="<?= \Volnix\CSRF\CSRF::getToken() ?>"/>
+	<input type="text" name="action" placeholder="Enter an action."/>
+	<input type="submit" value="Submit" name="sub"/>
+</form>
+
+```
+
+Hidden input string:
+
+```php
+
+<form action="index.php" method="post">
+	<?= \Volnix\CSRF\CSRF::getAsHiddenInput() ?>
+	<input type="text" name="action" placeholder="Enter an action."/>
+	<input type="submit" value="Submit" name="sub"/>
+</form>
+
+```
+
+Query string:
+
+```php
+
+<form action="index.php?<?= \Volnix\CSRF\CSRF::getQueryString() ?>" method="get">
+	<input type="text" name="action" placeholder="Enter an action."/>
+	<input type="submit" value="Submit" name="sub"/>
+</form>
+
+```
+
+If a different token name is desired, you may pass it in on any call that retrieves the token (`getToken()`, `getHiddenInputString()`, `getQueryString()`):
+
+```php
+
+<form action="index.php" method="post">
+	<?= \Volnix\CSRF\CSRF::getAsHiddenInput('custom_token_name') ?>
+	<input type="text" name="action" placeholder="Enter an action."/>
+	<input type="submit" value="Submit" name="sub"/>
+</form>
+
+```
---
+++ tests/CSRF.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace INA\Security\Tests;
+use Volnix\CSRF\CSRF as CSRFTokenGenerator;
+
+class CSRF extends \PHPUnit_Framework_TestCase
+{
+	const ALT_TOKEN_NAME = "foo_bar_1234567890";
+
+	public function __construct()
+	{
+		ob_start();
+	}
+	
+	public function __destruct()
+	{
+		print ob_get_clean();
+	}
+
+	public function setUp()
+	{
+		// Suppress errors on session_start() in test
+		// http://stackoverflow.com/questions/23270650/cannot-send-session-cookie-headers-already-sent-phpunit-laravel
+		@session_start();
+		$this->_killSession();
+	}
+
+	public function tearDown()
+	{
+		$this->_killSession();
+		session_write_close();
+	}
+	
+	public function testGenerateToken()
+	{
+		CSRFTokenGenerator::generateToken();
+		$this->assertNotNull($_SESSION[CSRFTokenGenerator::TOKEN_NAME]);
+
+		CSRFTokenGenerator::generateToken(self::ALT_TOKEN_NAME);
+		$this->assertNotNull($_SESSION[self::ALT_TOKEN_NAME]);
+	}
+
+	public function testGetToken()
+	{
+		// basic
+		CSRFTokenGenerator::generateToken();
+		$this->assertEquals($_SESSION[CSRFTokenGenerator::TOKEN_NAME], CSRFTokenGenerator::getToken());
+
+		CSRFTokenGenerator::generateToken(self::ALT_TOKEN_NAME);
+		$this->assertEquals($_SESSION[self::ALT_TOKEN_NAME], CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME));
+
+		$this->_killSession();
+
+		// no token set, should get set without explicit generate call
+		$this->assertNotNull(CSRFTokenGenerator::getToken());
+		$this->assertNotNull(CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME));
+
+		$this->assertEquals($_SESSION[CSRFTokenGenerator::TOKEN_NAME], CSRFTokenGenerator::getToken());
+		$this->assertEquals($_SESSION[self::ALT_TOKEN_NAME], CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME));
+	}
+
+	public function testValidate()
+	{
+		// good validation
+		$post_data = [CSRFTokenGenerator::TOKEN_NAME => CSRFTokenGenerator::getToken()];
+		$this->assertTrue(CSRFTokenGenerator::validate($post_data));
+
+		$post_data = [self::ALT_TOKEN_NAME => CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME)];
+		$this->assertTrue(CSRFTokenGenerator::validate($post_data, self::ALT_TOKEN_NAME));
+
+		// bad validation
+		$this->_killSession();
+
+		$post_data = [CSRFTokenGenerator::TOKEN_NAME => "bad_token_data"];
+		$this->assertFalse(CSRFTokenGenerator::validate($post_data));
+
+		$post_data = [self::ALT_TOKEN_NAME => "bad_token_data"];
+		$this->assertFalse(CSRFTokenGenerator::validate($post_data, self::ALT_TOKEN_NAME));
+
+		$post_data = ["bad_token_name" => CSRFTokenGenerator::getToken()];
+		$this->assertFalse(CSRFTokenGenerator::validate($post_data));
+	}
+
+	public function testGetHiddenInput()
+	{
+		$token = CSRFTokenGenerator::getToken();
+		$this->assertEquals(sprintf('<input type="hidden" name="%s" value="%s"/>', CSRFTokenGenerator::TOKEN_NAME, $token), CSRFTokenGenerator::getHiddenInputString());
+
+		$token = CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME);
+		$this->assertEquals(sprintf('<input type="hidden" name="%s" value="%s"/>', self::ALT_TOKEN_NAME, $token), CSRFTokenGenerator::getHiddenInputString(self::ALT_TOKEN_NAME));
+	}
+
+	public function testGetQueryString()
+	{
+		$token = CSRFTokenGenerator::getToken();
+		$this->assertEquals(sprintf('%s=%s', CSRFTokenGenerator::TOKEN_NAME, $token), CSRFTokenGenerator::getQueryString());
+
+		$token = CSRFTokenGenerator::getToken(self::ALT_TOKEN_NAME);
+		$this->assertEquals(sprintf('%s=%s', self::ALT_TOKEN_NAME, $token), CSRFTokenGenerator::getQueryString(self::ALT_TOKEN_NAME));
+	}
+
+	private function _killSession()
+	{
+		$_SESSION[CSRFTokenGenerator::TOKEN_NAME] = $_SESSION[self::ALT_TOKEN_NAME] = null;
+		unset($_SESSION[CSRFTokenGenerator::TOKEN_NAME], $_SESSION[self::ALT_TOKEN_NAME]);
+	}
+}
